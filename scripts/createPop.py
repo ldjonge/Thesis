e7594e4df3defec898f3a97ea19580b81c5c49af
Lorenzo <ldjonge96@gmail.com>
2019-03-12 21:33:09 +0100

    updated frequency dependence

--- scripts/createPop.py
+++ scripts/createPop.py
@@ -2,28 +2,41 @@
 import random
 from standardFunctions import *
 from numpy.random import choice
+from numpy import exp
 import sys
 
 phenoDict = {"pp":"A", "pq":"A", "pr":"A", "qp":"A", "rp":"A", "qq":"I", "qr":"I", "rq":"I", "rr":"O"}
 
 #Starting parameters
-N = 200
-p=2/6
-q=2/6
-r=1/3
-nGen = 100
+N=100 #Population size, should be possible to keep small
+p=2/6 #Allele frequency
+q=2/6 #Allele frequency
+r=1/3 #Allele frequency
+nGen = 100 #Number of generations
+
+s = -2 #Selection Pressure
+equilA = 0.6 #Equilibrium frequency for andromorphs
+equilI = 0.15 #Equilibrium frequency for infuscans
 
 class Male:
     def __init__(self, pAll, mAll):
         self.genotype = pAll+mAll
         self.phenotype = "M"
+    def calcFec(self, phenoFreq):
         self.fecundity = 1
 
 class Female:
-    def __init__(self,pAll,mAll, phenoFreq):
+    def __init__(self,pAll,mAll):
         self.genotype = pAll+mAll
         self.phenotype = phenoDict[self.genotype]
-        self.fecundity = calcNFDF(self.phenotype, phenoFreq)
+
+    def calcFec(self, phenoFreq): #As per Le Rouzic 2015
+        if self.phenotype == "O":
+            self.fecundity = exp(0)
+        elif self.phenotype == "I":
+            self.fecundity = exp(s*(phenoFreq["I"] - equilI))
+        elif self.phenotype == "A":
+            self.fecundity = exp(s*(phenoFreq["A"] - equilA))
 
 def randomAllele(p,q,r):
     newRand = random.random()
@@ -45,17 +58,17 @@ def createMalePop(N, p, q, r):
         population.append(Male(pAll,mAll))
     return(population)
 
-def createFemalePop(N, p, q, r, phenoFreq):
+def createFemalePop(N, p, q, r):
     population = []
     pAll = randomAllele(p,q,r)
     mAll = randomAllele(p,q,r)
     for i in range(N):
         pAll = randomAllele(p,q,r)
         mAll = randomAllele(p,q,r)
-        population.append(Female(pAll,mAll, phenoFreq))
+        population.append(Female(pAll,mAll))
     return(population)
 
-def reproduce(male, female, phenoFreq):
+def reproduce(male, female):
     if random.random() <= 0.5:
         pAll = male.genotype[0]
     else:
@@ -67,7 +80,7 @@ def reproduce(male, female, phenoFreq):
     if random.random() <= 0.5:
         return Male(pAll, mAll)
     else:
-        return Female(pAll, mAll, phenoFreq)
+        return Female(pAll, mAll)
 
 def newGen(pop):
     newMalePop = []
@@ -78,7 +91,7 @@ def newGen(pop):
     for i in range(N):
         pat = choice(pop[0], 1, maleFec)[0]
         mat = choice(pop[1], 1, femaleFec)[0]
-        offspring = reproduce(pat, mat, phenoFreq)
+        offspring = reproduce(pat, mat)
         if type(offspring) == Male:
             newMalePop.append(offspring)
         else:
@@ -135,17 +148,29 @@ def calcNFDF(phenotype, phenoFreq):
     return fecundity
 
 if __name__ == "__main__":
-    phenFreq = phenoFreq(p,q,r)
     malePop = createMalePop(N//2, p,q,r)
-    femalePop = createFemalePop(N//2, p,q,r, phenFreq)
+    femalePop = createFemalePop(N//2, p,q,r)
+    phenFreq = calcPhenoFreq((malePop, femalePop))
+    for ind in malePop:
+        ind.calcFec(phenFreq)
+    for ind in femalePop:
+        ind.calcFec(phenFreq)
     pop = (malePop, femalePop)
     freqTable = [["A", "I", "O"]]
     for gen in range(nGen):
         pop = newGen(pop)
         phenFreq = calcPhenoFreq(pop)
+        for ind in pop[0]:
+            ind.calcFec(phenFreq)
+        for ind in pop[1]:
+            ind.calcFec(phenFreq)
         freqTable.append([str(phenFreq["A"]), str(phenFreq["I"]), str(phenFreq["O"])])
     genoFreq = calcGenoFreq(pop)
-    with open(sys.argv[1], 'w') as outfile:
+    if len(sys.argv) > 1:
+        with open(sys.argv[1], 'w') as outfile:
+            for line in freqTable:
+                print("\t".join(line), file=outfile)
+    else:
         for line in freqTable:
-            print("\t".join(line), file=outfile)
+            print("\t".join(line))
     #print(genoFreq)
